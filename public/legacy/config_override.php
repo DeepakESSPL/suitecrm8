d97d021c024a726bc3e742df764d2f357588e691
Deepak Singh Kushwah <deepak@esspl.co.uk>
2026-02-06 15:04:04 +0000

    Add .gitignore and config_override.php for local development

--- public/legacy/config_override.php
+++ public/legacy/config_override.php
@@ -4,3 +4,15 @@ $sugar_config['default_export_charset'] = 'ISO-8859-1';
 $sugar_config['verify_client_ip'] = false;
 $sugar_config['system_name'] = 'TableReadyCRM';
 /***CONFIGURATOR***/
+
+// Local development database configuration
+// These settings override config.php for local development only
+$sugar_config['dbconfig']['db_host_name'] = '127.0.0.1';
+$sugar_config['dbconfig']['db_user_name'] = 'root';
+$sugar_config['dbconfig']['db_password'] = 'deepak';
+$sugar_config['dbconfig']['db_name'] = 'suitecrm8';
+$sugar_config['dbconfig']['db_port'] = '3306';
+
+// Local site URL
+$sugar_config['site_url'] = 'http://suitecrm8.local';
+$sugar_config['host_name'] = 'suitecrm8.local';
